a80a21f835b2fe2bbdedb65281538acd3013ab8b
Pelin Hamdemir <160609031+pelinhamdemir@users.noreply.github.com>
2024-08-08 14:25:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, make_response 
-from flask_cors import CORS
+from flask_cors import 
 CORS app = Flask(__name__) 
 CORS(app, resources={r"/api/*": {"origins": "https://pelinhamdemir.github.io"}}) 
 @app.route('/api/predict', methods=['POST', 'OPTIONS']) 
