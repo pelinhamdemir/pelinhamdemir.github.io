4aae7ba239a189064018d1282d5361c331f87715
Pelin Hamdemir <160609031+pelinhamdemir@users.noreply.github.com>
2024-08-08 11:31:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from flask_cors import CORS # type: ignore
 
 app = Flask(__name__)
 CORS(app)
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 @app.route('/api/predict', methods=['POST'])  # Ensure the method is POST
 def predict():
     data = request.get_json()  # Get the JSON data from the request
